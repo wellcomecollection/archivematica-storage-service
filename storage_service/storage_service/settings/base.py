cf95d8addf5cf7398d1697db2e05c51899959ce4
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2019-11-08 12:07:14 +0000

    Add OIDC login link

--- storage_service/storage_service/settings/base.py
+++ storage_service/storage_service/settings/base.py
@@ -166,6 +166,7 @@ TEMPLATES = [
                 "django.template.context_processors.tz",
                 "django.template.context_processors.request",
                 "django.contrib.messages.context_processors.messages",
+                "common.context_processors.auth_methods",
             ],
             "debug": DEBUG,
         },
